e6aea6712418dddbce03fc7cac6e32885bf2186c
f(x) - Cognition, Synthesis, Expression <david@fatehks.com>
2026-09-24 08:44:07 -0400

    Fix URL in installation instructions for thinkscript-mode

--- thinkscript-mode.el
+++ thinkscript-mode.el
@@ -19,7 +19,7 @@
 ;; To install it as your major mode for Thinkscript editing:
 
 ;; (use-package thinkscript-mode
-;;   :vc (:url "https://github.com/fatehks/thinkscript-mode/thinkscript-mode.el" :rev :newest))
+;;   :vc (:url "https://github.com/fatehks/thinkscript-mode" :rev :newest))
 
 ;;; Code:
 
